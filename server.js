a0134a1c4dff0b955fe2b84c4307095e0a991f40
Tracey Saada <tracey.saada@outlook.com>
2021-02-15 21:38:18 +0000

    initial set up of all folders & files

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const routes = require("./controllers/pantry-controller");
+const expressHandleBars = require("express-handlebars");
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
+app.use(routes);
+app.engine("handlebars", expressHandleBars({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+
+
+app.listen(PORT, () => console.log("aplication is running" + PORT));
