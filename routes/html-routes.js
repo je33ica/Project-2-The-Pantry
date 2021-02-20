a40a3407df5adc327ff56c9c32340d016f03a362
Babak Bigdeli <babakbigdeli@gmail.com>
2021-02-20 16:12:05 +0000

    updated html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,20 +1,44 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
-
+const db = require("../models");
 module.exports = function(app) {
-  app.get("/", (req, res) => {
-    res.render("index");
+  app.get("/foods", (req, res) => {
+    db.Food.findAll({}).then((foodResp) => {
+      res.json(foodResp);
+    });
   });
   app.get("/add", (req, res) => {
     res.render("add");
   });
+  //shoudl be returning JSON
   app.get("/perishable", (req, res) => {
-    res.render("perishable");
+    db.Food.find({
+      where: {
+        storageCondition: "perishable",
+      },
+    }).then((foodResp) => {
+      res.render("perishable", { food: foodResp });
+      console.log("response in api route", foodResp);
+    });
   });
   app.get("/frozen", (req, res) => {
-    res.render("frozen");
+    db.Food.findAll({
+      where: {
+        storageCondition: "frozen",
+      },
+    }).then((foodResp) => {
+      res.render("frozen", { food: foodResp });
+      console.log("response in api route", foodResp);
+    });
   });
   app.get("/dry", (req, res) => {
-    res.render("dry");
+    db.Food.findAll({
+      where: {
+        storageCondition: "dry",
+      },
+    }).then((foodResp) => {
+      res.render("dry", { food: foodResp });
+      console.log("response in api route", foodResp);
+    });
   });
 };
