70cee2139bdc21acd3a0015c6cc1207c6df28890
Babak Bigdeli <babakbigdeli@gmail.com>
2021-02-20 16:11:49 +0000

    Trimmed api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,49 +1,6 @@
 const db = require("../models");
-
 //GET request, find all food data.
 module.exports = function(app) {
-  app.get("/api/foods", (req, res) => {
-    db.Food.findAll({}).then((foodResp) => {
-      res.json(foodResp);
-    });
-  });
-
-  app.get("/api/perishable", (req, res) => {
-    db.Food.findAll({
-      where: {
-        storageCondition: "perishable",
-      },
-    }).then((foodResp) => {
-      res.render("perishable", { food: foodResp });
-      console.log("response in api route", foodResp);
-      //   console.log("response in api route", { Food: dataValues });
-    });
-  });
-
-  app.get("/api/frozen", (req, res) => {
-    db.Food.findAll({
-      where: {
-        storageCondition: "frozen",
-      },
-    }).then((foodResp) => {
-      res.render("frozen", { food: foodResp });
-      console.log("response in api route", foodResp);
-      //   console.log("response in api route", { Food: dataValues });
-    });
-  });
-
-  app.get("/api/dry", (req, res) => {
-    db.Food.findAll({
-      where: {
-        storageCondition: "dry",
-      },
-    }).then((foodResp) => {
-      res.render("dry", { food: foodResp });
-      console.log("response in api route", foodResp);
-      //   console.log("response in api route", { Food: dataValues });
-    });
-  });
-
   //POST request, add food item to database.
   app.post("/api/foods", (req, res) => {
     const foods = {
@@ -54,7 +11,6 @@ module.exports = function(app) {
       category: req.body.category,
       price: req.body.price,
     };
-
     db.Food.create(foods).then((foodResp) => {
       res.json(foodResp);
     });
