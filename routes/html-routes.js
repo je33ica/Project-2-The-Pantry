e74cc869023471de6fb39c6f2a96c8d35c62577f
je33ica <je331687@gmail.com>
2021-02-20 13:46:16 +0000

    added get route for perishable

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,15 +1,14 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
 
-module.exports = function (app) {
+module.exports = function(app) {
   app.get("/", (req, res) => {
-    res.render("index")
-    }
-  )
+    res.render("index");
+  });
   app.get("/add", (req, res) => {
-    res.render("add")
-    }
-    
-  )
+    res.render("add");
+  });
+  app.get("/perishable", (req, res) => {
+    res.render("perishable");
+  });
 };
-  
