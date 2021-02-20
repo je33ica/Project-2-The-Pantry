d7a518b53ee15ce5e0dc80c75d415027c49a7e47
Harry Stead <harrystead9@gmail.com>
2021-02-20 09:44:01 +0000

    data is showing

--- public/chart.js
+++ public/chart.js
@@ -1,29 +1,42 @@
+getData();
+
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
-  type: 'doughnut',
+  type: "doughnut",
   data: {
-    labels: ['DRY', 'PERISHABLE', 'FROZEN'],
-    datasets: [{
-      label: '# of food',
-      data: [12, 19, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.5)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)'
-      ],
-      borderWidth: 1
-    }]
+    labels: ["DRY", "PERISHABLE", "FROZEN"],
+    datasets: [
+      {
+        label: "# of food",
+        data: [12, 19, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.5)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255,99,132,1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
   },
   options: {
-   	//cutoutPercentage: 40,
+    //cutoutPercentage: 40,
     responsive: false,
-
-  }
+  },
 });
+
+function getData() {
+  let myFetch = fetch("http://localhost:3000/api/foods");
+
+  myFetch.then(function(response) {
+    response.json().then(function(text) {
+      console.log(text);
+    });
+  });
+}
