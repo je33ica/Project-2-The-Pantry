857c30b17229302e21fe3af034e7589843a83f36
Tracey Saada <tracey.saada@outlook.com>
2021-02-20 14:31:02 +0000

    removed unused code

--- public/perishable.js
+++ public/perishable.js
@@ -1,15 +1,3 @@
-// fetch("/api/perishable", {
-//   method: "GET",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   response,
-// })
-//   .then((response) => {
-//     console.log("response in perishable.js", response);
-//     location.reload();
-//   })
-//   .catch((err) => console.log(err));
 
 function getData() {
   let myFetch = fetch("http://localhost:3000/api/foods");
