dd97899b47cdc2efe78f2d56696b0d7cf3376ca8
Harry Stead <harrystead9@gmail.com>
2021-02-22 12:49:55 +0000

    bar chart functional, dates in order, values valid, current date value yet to be set

--- public/chart.js
+++ public/chart.js
@@ -6,6 +6,7 @@ let frozenArray = [];
 
 async function chartIt() {
   await getData();
+  console.log(dryArray);
   const ctx = document.getElementById("myChart");
   console.log(dryArray.length);
   const myDoughnutChart = new Chart(ctx, {
@@ -39,10 +40,7 @@ async function chartIt() {
   });
 }
 
-
-
 async function getData() {
-
   let myFetch = await fetch("http://localhost:3000/api/foods");
   const data = await myFetch.json();
   console.log(data);
@@ -57,6 +55,77 @@ async function getData() {
       frozenArray.push(numOfItem);
     }
   }
+}
+
+barChartIt();
+
+let dateKeys = [];
+let dateValues =[];
+let todaysDate;
+let todaysDateValue;
+
+async function barChartIt() {
+  await getDataTwo();
+  console.log(dateKeys);
+  new Chart(document.getElementById("bar-chart"), {
+    type: "bar",
+    data: {
+      labels: [todaysDate + " (today)", ...dateKeys],
+      datasets: [
+        {
+          label: "No. of items for each expiry date",
+          backgroundColor: [
+            "#3e95cd",
+            "#8e5ea2",
+            "#3cba9f",
+            "#e8c3b9",
+            "#c45850",
+          ],
+          data: [0, ...dateValues],
+        },
+      ],
+    },
+    options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: "Number of Items by Expiry Date",
+      },
+    },
+  });
+}
+
+const datesArray = [];
+async function getDataTwo() {
+  let myFetch = await fetch("http://localhost:3000/api/foods");
+  const data = await myFetch.json();
+  console.log(data);
+
+  for (var i = 0; i < data.length; i++) {
+    datesArray.push(data[i].expiration);
+  }
+  var occurrences = {};
+  const array = [];
+
+  for (var i = 0, j = datesArray.length; i < j; i++) {
+    occurrences[datesArray[i]] = (occurrences[datesArray[i]] || 0) + 1;
+  }
+
+  console.log(occurrences)
+  const objectArray = Object.entries(occurrences).sort();
+
+  objectArray.forEach(([key, value]) => {
+    console.log(key); 
+    dateKeys.push(key)
+    dateValues.push(value)
+    console.log(value); 
+  });
+
+  todaysDate = moment().format('YYYY-MM-DD');
+  // dateKeys = Object.keys(occurrences).sort();
+  // console.log(dateKeys);
+  // dateValues = Object.values(occurrences);
+
 }
 
 
